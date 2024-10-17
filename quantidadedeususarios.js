1ccd56b8d063be89ed9ecddbb777e011b7f991cd
daniel <102987388+danielorloski@users.noreply.github.com>
2024-10-17 11:24:04 -0300

    Add files via upload

--- quantidadedeususarios.js
+++ quantidadedeususarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./common.js";
+import { getCSS, tickConfig  } from "./common.js";
 
 
 async function quantidadeUsuarios() {
@@ -19,7 +19,46 @@ async function quantidadeUsuarios() {
       }
     }
   ]
-  
+
+  const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+      text: 'Redes sociais com mais usuários no mundo',
+       x: 0,
+       font: {
+           color: getCSS('--primary-color'),
+           family: getCSS('--font'),
+           size: 30
+       }
+    },
+    
+  xaxis: {
+    tickfont: tickConfig, 
+    {
+      color: getCSS('--primary-color')
+      size: 16
+      family: getCSS('--font')
+    }
+      title: {
+          text: 'nome das redes sociais',
+          font: {
+              color: getCSS('--secondary-color')
+          }
+      }
+  },
+  yaxis: {
+    tickfont: tickConfig, 
+          title: {
+          text: 'bilhões de usuários ativos',
+          font: {
+              color: getCSS('--secondary-color')
+          }
+      }
+  }
+}
+  }
+
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
   document.getElementById('graficos-container').appendChild(grafico)
